b3d4346271fd73f64f81144944a92af61ceea3de
Prasanna2712 <prasanna.diwan27@gmail.com>
2025-05-05 09:41:21 +0530

    Add files via upload

---
+++ Splash.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame implements Runnable
+{
+    Thread t;
+    Splash()
+    {
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/enhanced_logo.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900, 600, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image);
+
+        t = new Thread(this);
+        t.start();
+        setVisible(true);
+
+        int x = 1;
+        for (int i = 2; i <= 600; i+=4,x+=1)
+        {
+            setLocation(600-((i+x)/2),350-(i/2));
+            setSize(i+3*x, i+x/2);
+
+            try
+            {
+                Thread.sleep(10);
+            }
+            catch(Exception e)
+            {
+                System.out.print(" ");
+            }
+            }
+    }
+    public void run()
+    {
+        try
+        {
+            Thread.sleep(4000);
+            setVisible(false);
+            // Next Frame
+            new Login();
+        }
+        catch(Exception e)
+        {
+            System.out.print("");
+        }
+    }
+    public static void main(String []args)
+    {
+       new Splash();
+    }
+}
